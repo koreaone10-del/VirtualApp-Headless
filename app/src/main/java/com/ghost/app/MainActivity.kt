77340a3df03ac63f23a137d9a1c8298ac3644251
koreaone10-del <koreaone10@gmail.com>
2026-08-10 16:20:04 +0100

    Add MainActivity with overlay permission handling

---
+++ app/src/main/java/com/ghost/app/MainActivity.kt
@@ -0,0 +1,26 @@
+```kotlin
+package com.ghost.app
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (!Settings.canDrawOverlays(this)) {
+            startActivity(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")))
+            finish()
+            return
+        }
+        val serviceIntent = Intent(this, GhostService::class.java).apply {
+            putExtra("TARGET_PACKAGE", "com.example.yourapp") // غيره إلى حزمة تطبيقك
+        }
+        startForegroundService(serviceIntent)
+        finish()
+    }
+}
+```
